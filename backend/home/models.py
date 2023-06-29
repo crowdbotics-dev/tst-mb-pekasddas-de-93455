5afb76f820b717a6545ec8e9beba4e8be51b9497
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-29 17:10:34 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.db import models
 class Hello(models.Model):
     'Generated Model'
-    addsdd = models.BigIntegerField()
-    klklkjj = models.BigIntegerField(null=True,blank=True,)
+    addsdd = models.BigIntegerField(blank=True,)
+    klklkjjrddddds = models.BigIntegerField(null=True,blank=True,)
 class Hii(models.Model):
     'Generated Model'
     dsdds = models.BigIntegerField()
